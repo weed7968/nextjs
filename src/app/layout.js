9ebddc53a31152abe07591080c349138cdde9c1c
weed7968 <tlstjdcjf980809@gmail.com>
2024-02-01 23:10:49 +0900

    오류 수정

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import "./globals.css";
-import { useEffect, useState } from "react";
 
 export const metadata = {
   title: "Web tutorials",
@@ -8,14 +7,6 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  const [topics, setTopics] = useState([]);
-  useEffect(() => {
-    fetch("http://localhost:9999/topics")
-      .then((resp) => resp.json())
-      .then((result) => {
-        setTopics(result);
-      });
-  });
   return (
     <html>
       <body>
