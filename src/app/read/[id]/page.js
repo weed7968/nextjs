d78f4bbb2a9177dc55cb925a608b996fbb48d506
weed7968 <tlstjdcjf980809@gmail.com>
2024-02-05 21:54:09 +0900

    글 읽기

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,7 +1,10 @@
-export default function Read(props) {
+export default async function Read(props) {
+  const resp = await fetch(`http://localhost:9999/topics/${props.params.id}`);
+  const topic = await resp.json();
   return (
     <>
       <h2>Read</h2>
+      {topic.body}
       paramters : {props.params.id}
     </>
   );
